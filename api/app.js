9581e68024abf3132c18a0ba785fb3bb5c6e6e6e
wonbeomcho <160467612+wonbeomcho@users.noreply.github.com>
2025-06-17 19:16:53 +0900

    0612

--- api/app.js
+++ api/app.js
@@ -1,18 +1,27 @@
-//https://crudcrud.com/api/de38a51c3d724718938a333e22a17f82
-
-// fetch("https://crudcrud.com/api/de38a51c3d724718938a333e22a17f82/post").then((res)=>{
-// return res.json()
-// }).then((json)=>{
-//     console.log(json)
-// })
-
-// async function init(){}
-const post_url = "https://crudcrud.com/api/de38a51c3d724718938a333e22a17f82/post"
+const post_url = "http://crud.tlol.me/wonbeom/post";
 const init = async ()=>{
     const res = await fetch(post_url);
     const json = await res.json();
     console.log(json);
+    json.forEach(post => {
+        const postLi = document.createElement("li");
+        postLi.innerText = post.title;
+        document.querySelector("#board").appendChild(postLi);
+        const contentDiv = document.createElement("div");
+        contentDiv.innerText = post.content;
+        postLi.appendChild(contentDiv);
+        contentDiv.style.display = 'none';
+        postLi.addEventListener('click',()=>{
+            if (contentDiv.style.display == 'block') {
+                contentDiv.style.display = 'none';
+            }else{
+                contentDiv.style.display = 'block';
+            }
+        });
+        
+    });
 }
+
 const write = async (data)=>{
     const res = await fetch(post_url,{
         method:"POST",
@@ -38,5 +47,13 @@ document.querySelector("#post-form").addEventListener("submit",(e)=>{
     e.preventDefault()
     submit();
 })
+
+document.querySelector("#toggle-write").addEventListener("click",()=>{
+    if(document.querySelector("#post-form").style.display == 'block'){
+        document.querySelector("#post-form").style.display = 'none'
+    }else{
+        document.querySelector("#post-form").style.display = 'block'
+    }
+});
 
 init()
